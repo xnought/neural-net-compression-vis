aad735c008ba7e7c8935ff67770b3a7ae904b269
xnought <bertuccd@oregonstate.edu>
2023-08-20 18:51:19 -0400

    add tarveersing

--- quantizer.py
+++ quantizer.py
@@ -58,8 +58,6 @@ def quantize_linear_layer(layer, k=8, dtype=torch.float32):
     new_bias = None
     if num_weights > total_bits:
         w_codebook, w_indexes = quantize(weight, k, dtype)
-        print(w_indexes.shape)
-        print(w_codebook.shape)
         new_weight = QuantizedParams(w_indexes, w_codebook)
     else:
         # no quantization :(
@@ -73,3 +71,19 @@ def quantize_linear_layer(layer, k=8, dtype=torch.float32):
         new_bias = RegularParams(bias if bias is not None else 0.0)
 
     return HybridLinear(new_weight, new_bias)
+
+
+def traverse_named_modules(m, filter="Linear"):
+    for name, l in m.named_modules():
+        if type(l).__name__ == filter:
+            sep_name = name.split(".")
+            parent = ".".join(sep_name[:-1])
+            child = sep_name[-1]
+            yield parent, child, l
+
+
+# for p, c, l in traverse_named_modules(pipe.unet):
+#     q_w = RegularParams(l.weight)
+#     q_b = RegularParams(0.0 if l.bias is None else l.bias)
+#     replace = HybridLinear(q_w, q_b, in_features=l.in_features, out_features=l.out_features)
+#     setattr(pipe.unet.get_submodule(p), c, replace)
