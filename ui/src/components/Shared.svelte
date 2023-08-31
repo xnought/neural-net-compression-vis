0349eb3b3abcb596667f9451bc3b53067cadb34e
xnought <bertuccd@oregonstate.edu>
2023-08-31 15:16:28 -0400

    fix if interpolation still happens

--- ui/src/components/Shared.svelte
+++ ui/src/components/Shared.svelte
@@ -63,7 +63,12 @@
 			const G = d[i + 1];
 			const B = d[i + 2];
 			const key = `rgb(${R},${G},${B})`;
-			json.push({ R, G, B, index: codebook[key] ?? -1 });
+			const index = codebook[key] ?? -1;
+			if (index > 8) {
+				// some browsers mess up
+				notWorking = true;
+			}
+			json.push({ R, G, B, index });
 		}
 		return json;
 	}
@@ -75,96 +80,106 @@
 
 	$: if (mounted && mouseX !== undefined && mouseY !== undefined)
 		read(mouseX, mouseY, windowSize);
+	let notWorking = false;
 </script>
 
 <div style="position: relative; display: flex; gap: 10px; align-items: center;">
-	<canvas bind:this={canvasEl} {width} {height} />
-	<svg
-		{width}
-		{height}
-		style="position: absolute; left: 0; top: 0; cursor: none;"
-		on:mousemove={(e) => {
-			mouseX = e.offsetX;
-			mouseY = e.offsetY;
-		}}
-	>
-		<rect
-			x={mouseX}
-			y={mouseY}
-			width={windowSize}
-			height={windowSize}
-			fill="none"
-			stroke={windowColor}
-			stroke-width={2}
-		/>
-	</svg>
-
-	<div>
-		<div style="color: {windowColor}; margin-bottom: 5px;">Magnified</div>
+	{#if notWorking}
+		Canvas context imageSmoothingEnabled not working or Canvas Pixelated CSS
+		property not working in your browser, try Chrome.
+	{:else}
+		<canvas bind:this={canvasEl} {width} {height} />
 		<svg
-			width={svgWidth}
-			height={svgHeight}
-			style="outline: {windowColor} solid 2px;"
+			{width}
+			{height}
+			style="position: absolute; left: 0; top: 0; cursor: none;"
+			on:mousemove={(e) => {
+				mouseX = e.offsetX;
+				mouseY = e.offsetY;
+			}}
 		>
-			{#if data}
-				{#each { length: windowSize } as _, i}
-					{#each { length: windowSize } as _, j}
-						{@const square = svgWidth / windowSize}
-						{@const d = data[j * windowSize + i]}
-						{@const color = `rgb(${d.R}, ${d.G}, ${d.B})`}
-						{@const textOffset = square / 2}
-						{@const textSize = 3}
-						{#if d.index !== -1}
-							<rect
-								x={i * square}
-								y={j * square}
-								width={square}
-								height={square}
-								fill={color}
-								stroke="white"
-								stroke-width={1}
-								stroke-opacity={0.2}
-							/>
-							<text
-								x={i * square + textOffset}
-								y={j * square + textOffset + textSize}
-								fill="white"
-								style="font-size: 10px; font-family: menlo; font-weight: 200;"
-								text-anchor="middle">{d.index}</text
-							>
-						{/if}
+			<rect
+				x={mouseX}
+				y={mouseY}
+				width={windowSize}
+				height={windowSize}
+				fill="none"
+				stroke={windowColor}
+				stroke-width={2}
+			/>
+		</svg>
+
+		<div>
+			<div style="color: {windowColor}; margin-bottom: 5px;">
+				Magnified
+			</div>
+			<svg
+				width={svgWidth}
+				height={svgHeight}
+				style="outline: {windowColor} solid 2px;"
+			>
+				{#if data}
+					{#each { length: windowSize } as _, i}
+						{#each { length: windowSize } as _, j}
+							{@const square = svgWidth / windowSize}
+							{@const d = data[j * windowSize + i]}
+							{@const color = `rgb(${d.R}, ${d.G}, ${d.B})`}
+							{@const textOffset = square / 2}
+							{@const textSize = 3}
+							{#if d.index !== -1}
+								<rect
+									x={i * square}
+									y={j * square}
+									width={square}
+									height={square}
+									fill={color}
+									stroke="white"
+									stroke-width={1}
+									stroke-opacity={0.2}
+								/>
+								<text
+									x={i * square + textOffset}
+									y={j * square + textOffset + textSize}
+									fill="white"
+									style="font-size: 10px; font-family: menlo; font-weight: 200;"
+									text-anchor="middle">{d.index}</text
+								>
+							{/if}
+						{/each}
 					{/each}
+				{/if}
+			</svg>
+		</div>
+
+		indexes into →
+
+		<div>
+			{#if codebook}
+				<b>Shared Weights</b>
+				<br />
+				{#each Object.keys(codebook) as item, i}
+					{#if i < 8}
+						<div style="width: 160px; ">
+							<span style="font-family: menlo;"
+								>{codebook[item]}</span
+							>
+							<div
+								style="background: {item}; color: white; display: inline;"
+							>
+								{item}
+							</div>
+						</div>
+					{/if}
 				{/each}
 			{/if}
-		</svg>
-	</div>
-
-	indexes into →
-
-	<div>
-		{#if codebook}
-			<b>Shared Weights</b>
-			<br />
-			{#each Object.keys(codebook) as item, i}
-				{#if i < 8}
-					<div style="width: 160px; ">
-						<span style="font-family: menlo;">{codebook[item]}</span
-						>
-						<div
-							style="background: {item}; color: white; display: inline;"
-						>
-							{item}
-						</div>
-					</div>
-				{/if}
-			{/each}
-		{/if}
-	</div>
+		</div>
+	{/if}
 </div>
 
 <style>
 	canvas {
 		border-radius: 5px;
 		box-shadow: 0px 0px 2px 2px #00000020;
+		image-rendering: pixelated;
 	}
 </style>
